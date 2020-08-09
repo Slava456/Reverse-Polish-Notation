4a64e2acbbded468516e224cb94113517a03cd35
Slava456 <67310601+Slava456@users.noreply.github.com>
2020-08-09 19:11:52 +0400

    Refactor GetPriority()

--- RPNCountClass.cs
+++ RPNCountClass.cs
@@ -27,13 +27,13 @@ namespace RPNLib
         {
             switch (s)
             {
-                case '(': return 0;
-                case ')': return 1;
-                case '+': return 2;
-                case '-': return 3;
-                case '*': return 4;
-                case '/': return 4;
-                case '^': return 5;
+                case _openBreaker: return 0;
+                case _closedBreaker: return 1;
+                case _plus: return 2;
+                case _minus: return 3;
+                case _multiply: return 4;
+                case _divis: return 4;
+                case _exponent: return 5;
                 default: return 6;
             }
         }
